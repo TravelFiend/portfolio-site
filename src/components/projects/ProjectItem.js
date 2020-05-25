6392c22c1e2b79c8fbd2f7266cecf26cf5798468
TravelFiend <mikegrace274@gmail.com>
2020-05-24 20:35:01 -0700

    basic projectItem component made

--- src/components/projects/ProjectItem.js
+++ src/components/projects/ProjectItem.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import styles from './ProjectItem.css';
+import PropTypes from 'prop-types';
 
-const ProjectItem = () => {
+const ProjectItem = ({ projectName, picList, displayedPic }) => {
+  const snapshots = picList.map((onePic, i) => (
+    <li key={i}>
+      <img src={onePic}/>
+    </li>
+  ));
+
   return (
     <section className={styles.ProjectItem}>
-      <div></div>
-      <div></div>
+      <div>
+        <p>{projectName}</p>
+        <img src={displayedPic}/>
+      </div>
+
+      <ul>{snapshots}</ul>
     </section>
   );
+};
+
+ProjectItem.propTypes = {
+  projectName: PropTypes.string.isRequired,
+  picList: PropTypes.array.isRequired,
+  displayedPic: PropTypes.bool
 };
 
 export default ProjectItem;
---
+++ src/data/projectPicList.js
@@ -0,0 +1,50 @@
+import columnsHome from '../../assets/columnsHome.png';
+import columnsLogin from '../../assets/columnsLogin.png';
+import columnsGame from '../../assets/columnsGame.png';
+import columnsAbout from '../../assets/columnsAbout.png';
+import covrLogin from '../../assets/covrLogin.png';
+import covrForm from '../../assets/covrForm.png';
+import covrLetter from '../../assets/covrLetter.png';
+import covrAbout from '../../assets/covrAbout.png';
+import kitHome from '../../assets/kitHome.png';
+import kitSettings from '../../assets/kitSettings.png';
+import kitList from '../../assets/kitList.png';
+import nmHome from '../../assets/nmHome.png';
+import nmButtons from '../../assets/nmButtons.png';
+import nmCommState from '../../assets/nmCommState.png';
+import nmRecovery from '../../assets/nmRecovery.png';
+import nmCorona from '../../assets/nmCorona.png';
+import nmVideo from '../../assets/nmVideo.png';
+import nmAudio from '../../assets/nmAudio.png';
+import notableHome from '../../assets/notableHome.png';
+import notableLogin from '../../assets/notableLogin.png';
+import notableNotebooks from '../../assets/notableNotebooks.png';
+import notableEntries from '../../assets/notableEntries.png';
+import offbeatHome from '../../assets/offbeatHome.png';
+import offbeatLogin from '../../assets/offbeatLogin.png';
+import offbeatRecordings from '../../assets/offbeatRecordings.png';
+import offbeatAbout from '../../assets/offbeatAbout.png';
+
+const projectPics = [
+  {
+    projectName: 'Columns and Rogues',
+    imageList: [columnsHome, columnsLogin, columnsGame, columnsAbout]
+  }, {
+    projectName: 'CovrLettr',
+    imageList: [covrLogin, covrForm, covrLetter, covrAbout]
+  }, {
+    projectName: 'KIT (Keep In Touch)',
+    imageList: [kitHome, kitSettings, kitList]
+  }, {
+    projectName: 'NMConnect',
+    imageList: [nmHome, nmButtons, nmCommState, nmRecovery, nmCorona, nmVideo, nmAudio]
+  }, {
+    projectName: 'Notable Notes',
+    imageList: [notableHome, notableLogin, notableNotebooks, notableEntries]
+  }, {
+    projectName: 'Offbeat',
+    imageList: [offbeatHome, offbeatLogin, offbeatRecordings, offbeatAbout]
+  }
+];
+
+export default projectPics;
